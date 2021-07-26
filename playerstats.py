f7dedc2a51503d918c66273672cb9a3626b2738b
pranav2902 <34334397+pranav2902@users.noreply.github.com>
2021-07-26 17:58:41 +0530

    Added a common viz

--- playerstats.py
+++ playerstats.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
 import seaborn as sns
 
 ## Common Vizs - eFG%,  
@@ -9,6 +10,11 @@ import seaborn as sns
 ## SF Vizs - Shooting Graph, PPG Graph. Steals Graph, Rebounding Graph, Block Graph, 
 ## PF Vizs - Rebounding Graph, Block Graph, 
 ## C Vizs - Rebound graph. Block Graph
+
+
+## Current Issues
+## Special characters in names, not detected. Workaround : Type rest of name or part of the name without the accented letter or copy from online.
+
 
 
 ## Below function to preprocess Player names
@@ -32,20 +38,44 @@ def matchPlayerName(pl):
             print("Player found in dataset : {}".format(playertotals.Player[i]))
             f = 1
             break
+    ct = (playertotals["Player"] == playertotals.Player[i]).sum()
     if not f:
         print("Error: Player not found!") 
         return None,None
-    return i,playertotals.Player[i]
+    return i,playertotals.Player[i],ct
 
 
 ## Returns the playing position of the player
-def findPosition(pos,pl):
+def findPosition(pos):
     return playertotals.Pos[pos]  
+    
+
 
 ## Common Visualizations
-#def commonViz(pl):
-
-
+def commonViz(pl,pos,position,ct):
+    ## Quick plot of PPG vs FG%
+    ## For  positions
+    #style.use("seaborn-poster")
+    if(ct > 1):
+        fig,ax = plt.subplots(ct,1,figsize = (12*ct,12*ct),tight_layout = True)
+        sns.set_style("darkgrid")
+        for i in range(ct):
+            posn = playerpergame["Pos"][pos+i]
+            sns.scatterplot(data = playerpergame, x = playerpergame[playerpergame["Pos"] == posn]["PTS"],y = playerpergame[playerpergame["Pos"] == posn]["FG%"]*100,ax = ax[i],color = "g")
+            ax[i].set_title("PPG vs FG Percent for {} in {}".format(playerpergame["Player"][pos+i],playerpergame["Tm"][pos+i]))
+            ax[i].scatter(x = playerpergame["PTS"][pos+i],y = playerpergame["FG%"][pos+i]*100, color = "red")
+        plt.show()
+        plt.close()
+    else:
+        fig,ax = plt.subplots(ct,1,figsize = (20,20),tight_layout = True)
+        sns.set_style("darkgrid")
+        posn = playerpergame["Pos"][pos]
+        sns.scatterplot(data = playerpergame, x = playerpergame[playerpergame["Pos"] == posn]["PTS"],y = playerpergame[playerpergame["Pos"] == posn]["FG%"]*100,ax = ax,color = "g")
+        ax.set_title("PPG vs FG Percent for {} in {}".format(playerpergame["Player"][pos],playerpergame["Tm"][pos]))
+        ax.scatter(x = playerpergame["PTS"][pos],y = playerpergame["FG%"][pos]*100, color = "red")
+        plt.show()
+        plt.close()
+  
 
 
 
@@ -58,10 +88,11 @@ if __name__ == "__main__":
     newPlayerStats()
     print("Enter Player name with first letter in Caps")
     pl = input()
-    pos,pl = matchPlayerName(pl)
-    position = findPosition(pos,pl)
+    pos,pl,ct = matchPlayerName(pl)
+    position = findPosition(pos)
     print("His position is : {}".format(position))
-    #commonViz(pl)
+    commonViz(pl,pos,position,ct)
+
 
 
 
