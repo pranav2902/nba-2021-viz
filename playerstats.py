eb9cb09cfd8cf2b2df31deea445d332d394c3783
pranav2902 <34334397+pranav2902@users.noreply.github.com>
2021-07-26 21:34:09 +0530

    Basic webpage

--- playerstats.py
+++ playerstats.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import seaborn as sns
 
-## Common Vizs - eFG%,  
+## Common Vizs - PPG vs eFG%,  Make most of the stats here. Only specialized position vizs to be kept in different module
 ## PG Vizs - PPG,APG Graph. Shooting Graph. Steals Graph.   
 ## SG Vizs - Shooting Graph, PPG Graph. Steals Graph 
 ## SF Vizs - Shooting Graph, PPG Graph. Steals Graph, Rebounding Graph, Block Graph, 
@@ -26,7 +26,6 @@ def newPlayerStats():
         playerpergame["Player"][i] = playerpergame["Player"][i].split("\\")[0]
         playeradvanced["Player"][i] = playeradvanced["Player"][i].split("\\")[0]
         playershooting["Player"][i] = playershooting["Player"][i].split("\\")[0]
-
     return
 
 
@@ -56,6 +55,7 @@ def commonViz(pl,pos,position,ct):
     ## Quick plot of PPG vs FG%
     ## For  positions
     #style.use("seaborn-poster")
+    #print(ct)
     if(ct > 1):
         fig,ax = plt.subplots(ct,1,figsize = (12*ct,12*ct),tight_layout = True)
         sns.set_style("darkgrid")
@@ -64,6 +64,7 @@ def commonViz(pl,pos,position,ct):
             sns.scatterplot(data = playerpergame, x = playerpergame[playerpergame["Pos"] == posn]["PTS"],y = playerpergame[playerpergame["Pos"] == posn]["eFG%"]*100,ax = ax[i],color = "g")
             ax[i].set_title("PPG vs eFG Percent for {} in {}".format(playerpergame["Player"][pos+i],playerpergame["Tm"][pos+i]))
             ax[i].scatter(x = playerpergame["PTS"][pos+i],y = playerpergame["eFG%"][pos+i]*100, color = "red")
+            ax[i].text(x = playerpergame["PTS"][pos+i]+0.3,y = playerpergame["eFG%"][pos+i]*100+0.3,s=playerpergame.Player[pos+i])
         plt.show()
         plt.close()
     else:
@@ -73,16 +74,35 @@ def commonViz(pl,pos,position,ct):
         sns.scatterplot(data = playerpergame, x = playerpergame[playerpergame["Pos"] == posn]["PTS"],y = playerpergame[playerpergame["Pos"] == posn]["eFG%"]*100,ax = ax,color = "g")
         ax.set_title("PPG vs eFG Percent for {} in {}".format(playerpergame["Player"][pos],playerpergame["Tm"][pos]))
         ax.scatter(x = playerpergame["PTS"][pos],y = playerpergame["eFG%"][pos]*100, color = "red")
+        ax.text(x = playerpergame["PTS"][pos]+0.3,y = playerpergame["eFG%"][pos]*100+0.3,s=playerpergame.Player[pos])
         plt.show()
         plt.close()
+    ## Advanced stat Vizs
+    if(ct > 1):
+        fig,ax = plt.subplots(ct,1,figsize = (12*ct,12*ct),tight_layout = True)
+        sns.set_style("darkgrid")
+        for i in range(ct):
+            posn = playerpergame["Pos"][pos+i]
+            sns.scatterplot(data = playeradvanced, x = playeradvanced[playeradvanced["Pos"] == posn]["USG%"],y = playeradvanced[playeradvanced["Pos"] == posn]["TOV%"],ax = ax[i],color = "g")
+            ax[i].set_title("USG vs TOV Percent for {} in {}".format(playeradvanced["Player"][pos+i],playeradvanced["Tm"][pos+i]))
+            ax[i].scatter(x = playeradvanced["USG%"][pos+i],y = playeradvanced["TOV%"][pos+i], color = "red")
+            ax[i].text(x = playeradvanced["USG%"][pos+i]+0.3,y = playeradvanced["TOV%"][pos+i]+0.3, s=playeradvanced.Player[pos+i])
 
+        plt.show()
+        plt.close()
+    else:
+        fig,ax = plt.subplots(1,1,figsize = (12,12),tight_layout = True)
+        sns.set_style("darkgrid")
+        posn = playerpergame["Pos"][pos]
+        sns.scatterplot(data = playeradvanced, x = playeradvanced[playeradvanced["Pos"] == posn]["USG%"],y = playeradvanced[playeradvanced["Pos"] == posn]["TOV%"],ax = ax,color = "g")
+        ax.set_title("USG vs TOV Percent for {} in {}".format(playeradvanced["Player"][pos],playeradvanced["Tm"][pos]))
+        ax.scatter(x = playeradvanced["USG%"][pos],y = playeradvanced["TOV%"][pos], color = "red")
+        ax.text(x = playeradvanced["USG%"][pos]+0.3,y = playeradvanced["TOV%"][pos]+0.3, s=playeradvanced.Player[pos])
+        plt.show()
+        plt.close()
     
-  
-
-
 
 if __name__ == "__main__":
-
     playertotals = pd.read_excel("playerstats.xlsx",sheet_name="Player totals")
     playerpergame = pd.read_excel("playerstats.xlsx",sheet_name="Player per game")
     playeradvanced = pd.read_excel("playerstats.xlsx",sheet_name="Player Advanced")
